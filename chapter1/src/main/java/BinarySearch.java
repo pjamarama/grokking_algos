b87956e969910b7c9c3365ced82116f89268d31c
agrohotov <agrohotov@icerockdev.com>
2022-11-15 08:46:50 +0000

    Training binary search

--- chapter1/src/main/java/BinarySearch.java
+++ chapter1/src/main/java/BinarySearch.java
@@ -9,32 +9,33 @@ Outside the loop return -1.
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] array = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        bisearch(1, array);
+        int[] array = new int[] {3, 6, 8, 9, 11, 14, 16, 19, 22, 29};
+        System.out.println(bisearch(6, array));
     }
 
     private static int bisearch (int number, int[] array) {
-//        int step = 0;
-//        int startIdx = 0;
-//        int finIdx = array[array.length - 1];
-//
-//        while (startIdx <= finIdx) {
-//            ++step;
-//            int midIdx = (startIdx + finIdx) / 2;
-//            if (array[midIdx] == number) {
-//                System.out.println("Number found in " + step +" step(s)");
-//                return midIdx;
-//            }
-//
-//            if (array[midIdx] > number) {
-//                finIdx = midIdx - 1;
-//            } else {
-//                startIdx = midIdx + 1;
-//            }
-//        }
-//        return -1;
-//    }
+        int min = 0;
+        int max = array.length - 1;
+        int step = 0;
 
+        while (min <= max) {
+            int mid = (min + max) / 2;
+            ++step;
+
+            if (array[mid] == number) {
+                System.out.println("Found in " + step + " step(s)");
+                return mid;
+            }
+
+            if (number < array[mid]) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+
+        return -1;
+    }
 }
 
 
